4cca5dafa9f429315c8726e1e53837f43af2c4cd
Sergey Tregub <drwatson1@yandex.ru>
2019-02-13 15:09:16 +0300

    Fix options for status command

--- src/dbup-cli/CommandLineOptions/StatusOptions.cs
+++ src/dbup-cli/CommandLineOptions/StatusOptions.cs
@@ -1,16 +1,13 @@
 ﻿using CommandLine;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DbUp.Cli.CommandLineOptions
 {
     [Verb("status", HelpText = "Show upgrade status")]
     class StatusOptions: OptionsBase
     {
-        [Value(1, HelpText = "Print names of executed scripts", Default = false)]
+        [Option('e', "show-executed", HelpText = "Print names of executed scripts", Default = false)]
         public bool Executed { get; set; }
-        [Value(1, HelpText = "Print names of scripts to be execute", Default = false)]
+        [Option('n', "show-not-executed", HelpText = "Print names of scripts to be execute", Default = false)]
         public bool NotExecuted { get; set; }
     }
 }
