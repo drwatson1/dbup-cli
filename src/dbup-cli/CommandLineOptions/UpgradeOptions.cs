364bf6daa20e27f57ae101e06bd62afac22b8b85
Sergey Tregub <drwatson1@yandex.ru>
2019-03-21 17:06:04 +0300

    Fix option help string

--- src/dbup-cli/CommandLineOptions/UpgradeOptions.cs
+++ src/dbup-cli/CommandLineOptions/UpgradeOptions.cs
@@ -6,7 +6,7 @@ namespace DbUp.Cli.CommandLineOptions
     [Verb("upgrade", HelpText = "Upgrade database")]
     class UpgradeOptions: OptionsBase
     {
-        [Option("ensure", HelpText = "Create database if it is not exists")]
+        [Option("ensure", HelpText = "Create a database if not exists")]
         public bool Ensure { get; set; }
 
         [Option('e', "env", Required = false, HelpText = "Path to an environment file. Can be more than one file specified. The path can be absolute or relative against a current directory")]
