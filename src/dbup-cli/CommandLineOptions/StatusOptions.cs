9c59a0f801d43678a618dfb4e1d309bb0885df32
Sergey Tregub <drwatson1@yandex.ru>
2019-03-21 17:05:23 +0300

    Fix show-executed option abbreviation

--- src/dbup-cli/CommandLineOptions/StatusOptions.cs
+++ src/dbup-cli/CommandLineOptions/StatusOptions.cs
@@ -6,7 +6,7 @@ namespace DbUp.Cli.CommandLineOptions
     [Verb("status", HelpText = "Show upgrade status")]
     class StatusOptions: OptionsBase
     {
-        [Option('e', "show-executed", HelpText = "Print names of executed scripts", Default = false)]
+        [Option('x', "show-executed", HelpText = "Print names of executed scripts", Default = false)]
         public bool Executed { get; set; }
         [Option('n', "show-not-executed", HelpText = "Print names of scripts to be execute", Default = false)]
         public bool NotExecuted { get; set; }
