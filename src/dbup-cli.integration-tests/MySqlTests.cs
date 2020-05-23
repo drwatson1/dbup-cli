acab34b66a3c331b9e1738cf73709121cfcb947d
Sergey Tregub <sergey.tregub@vocord.ru>
2020-05-23 20:36:34 +0300

    Add MySql integration tests

---
+++ src/dbup-cli.integration-tests/MySqlTests.cs
@@ -0,0 +1,114 @@
+using DbUp.Cli.Tests.TestInfrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.Common;
+using MySql;
+using MySql.Data.MySqlClient;
+
+namespace DbUp.Cli.IntegrationTests
+{
+    [TestClass]
+    public class MySqlTests: DockerBasedTest
+    {
+        readonly CaptureLogsLogger Logger;
+        readonly IEnvironment Env;
+
+        readonly static string Pwd = "MyPwd2020";
+        readonly static string DbName = "DbUp";
+
+        public MySqlTests()
+        {
+            Env = new CliEnvironment();
+            Logger = new CaptureLogsLogger();
+
+            Environment.SetEnvironmentVariable("CONNSTR", $"Server=127.0.0.1;Database={DbName};Uid=root;Pwd={Pwd};");
+        }
+
+        string GetBasePath(string subPath = "EmptyScript") =>
+            Path.Combine(Assembly.GetExecutingAssembly().Location, $@"..\Scripts\MySQL\{subPath}");
+
+        string GetConfigPath(string name = "dbup.yml", string subPath = "EmptyScript") => new DirectoryInfo(Path.Combine(GetBasePath(subPath), name)).FullName;
+
+        Func<DbConnection> CreateConnection = () => new MySqlConnection($"Server=127.0.0.1;Uid=root;Pwd={Pwd};");
+
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await DockerInitialize(
+                "mysql:latest",
+                new List<string>()
+                {
+                    $"MYSQL_ROOT_PASSWORD={Pwd}"
+                },
+                "3306",
+                CreateConnection
+                );
+        }
+
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            await DockerCleanup(CreateConnection, con => new MySqlCommand("select count(*) from SchemaVersions where scriptname = '001.sql'", con as MySqlConnection));
+        }
+
+        [TestMethod]
+        public void Ensure_CreateANewDb()
+        {
+            var engine = new ToolEngine(Env, Logger);
+
+            var result = engine.Run("upgrade", "--ensure", GetConfigPath());
+            result.Should().Be(0);
+
+            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNSTR")))
+            using (var command = new SqlCommand("select count(*) from SchemaVersions where scriptname = '001.sql'", connection))
+            {
+                connection.Open();
+                var count = command.ExecuteScalar();
+
+                count.Should().Be(1);
+            }
+        }
+
+        [TestMethod]
+        public void Drop_DropADb()
+        {
+            var engine = new ToolEngine(Env, Logger);
+
+            engine.Run("upgrade", "--ensure", GetConfigPath());
+            var result = engine.Run("drop", GetConfigPath());
+            result.Should().Be(0);
+            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNSTR")))
+            using (var command = new SqlCommand("select count(*) from SchemaVersions where scriptname = '001.sql'", connection))
+            {
+                Action a = () => connection.Open();
+                a.Should().Throw<SqlException>($"Database {DbName} should not exist");
+            }
+        }
+
+        [TestMethod]
+        public void DatabaseShouldNotExistBeforeTestRun()
+        {
+            using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("CONNSTR")))
+            using (var command = new SqlCommand("select count(*) from SchemaVersions where scriptname = '001.sql'", connection))
+            {
+                Action a = () => connection.Open();
+                a.Should().Throw<SqlException>($"Database {DbName} should not exist");
+            }
+        }
+
+        [TestMethod]
+        public void UpgradeCommand_ShouldUseConnectionTimeoutForLongrunningQueries()
+        {
+            var engine = new ToolEngine(Env, Logger);
+
+            var r = engine.Run("upgrade", "--ensure", GetConfigPath("dbup.yml", "Timeout"));
+            r.Should().Be(1);
+        }
+    }
+}
